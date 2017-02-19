403255cb3d22c02565919356fb57f4dc1fc2d859
NadimRai <rai_nadim@yahoo.com>
2017-02-18 23:48:19 -0800

    functioning clock

--- scripts.js
+++ scripts.js
@@ -1,16 +1,17 @@
 function alarmClock(){
+	setInterval(clockUpdate,1000);	
+}
+
+function clockFormat(val){
+	if(val<10) val = "0"+val;	
+		return val;	
+}
+
+function clockUpdate(){
 	var date = new Date();
-	console.log(date);
-	console.log(date.getHours());
 	var clock = document.getElementById('clock');
 	clock.innerHTML = clockFormat(date.getHours())+ ":" + clockFormat(date.getMinutes()) + ":" + clockFormat(date.getSeconds());
 }
 
-function clockFormat(val){
-	if(val<10) val = "0"+val;
-		
-		return val;
-	
-}
 window.onload = alarmClock;
 
