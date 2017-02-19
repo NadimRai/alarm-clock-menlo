35e5f71b72a525f4009700fc557cd3d53401353a
NadimRai <rai_nadim@yahoo.com>
2017-02-18 23:43:49 -0800

    Formatted clock display time

--- scripts.js
+++ scripts.js
@@ -3,7 +3,14 @@ function alarmClock(){
 	console.log(date);
 	console.log(date.getHours());
 	var clock = document.getElementById('clock');
-	clock.innerHTML = date.getHours()+ ":" + date.getMinutes() + ":" + date.getSeconds();
+	clock.innerHTML = clockFormat(date.getHours())+ ":" + clockFormat(date.getMinutes()) + ":" + clockFormat(date.getSeconds());
+}
+
+function clockFormat(val){
+	if(val<10) val = "0"+val;
+		
+		return val;
+	
 }
 window.onload = alarmClock;
 
